3dc537eef9f39d7db0680c305d0b4244f46be801
FindTheBest <FindTheBest@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-30 17:48:43 +0000

    tagging version 3.0.9
    git-svn-id: https://plugins.svn.wordpress.org/findthebest-widget-designer/tags/3.0.9@1276541 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/tiny-mce-view.js
+++ js/tiny-mce-view.js
@@ -3,32 +3,37 @@
 	var ftbData = window.ftbData || {};
 	var version = +ftbData.wpVersion;
 
-	// View API changed significantly in 4.2
-	if ( version >= 4.2 ) {
+	$.each( ['findthebest', 'graphiq'], function( i, shortcodeLabel ) {
+		// View API changed significantly in 4.2
+		if (version >= 4.2) {
 
-		wp.mce.views.register( 'findthebest', {
-			template: wp.media.template( 'editor-findthebest' ),
-			getContent: function() {
-				return this.template( this.shortcode.attrs.named );
-			}
-		} );
-
-	// 3.9 - 4.1, use old view API
-	} else {
-
-		wp.mce.views.register( 'findthebest', {
-			View: {
+			wp.mce.views.register( shortcodeLabel, {
 				template: wp.media.template( 'editor-findthebest' ),
-				postID: $( '#post_ID' ).val(),
-				initialize: function( options ) {
-					this.shortcode = options.shortcode;
-				},
-				getHtml: function() {
-					return this.template( this.shortcode.attrs.named );
+				getContent: function () {
+					return this.template(this.shortcode.attrs.named);
 				}
-			}
-		} );
+			});
 
-	}
+			// 3.9 - 4.1, use old view API
+		} else {
+
+			wp.mce.views.register( shortcodeLabel, $.extend( {}, wp.mce.media, {
+				shortcode: shortcodeLabel,
+				edit: $.noop, // Override wp.mce.media
+				View: wp.mce.View.extend({
+					className: 'editor-findthebest',
+					template: wp.media.template( 'editor-findthebest' ),
+					postID: $('#post_ID').val(),
+					initialize: function (options) {
+						this.shortcode = options.shortcode;
+					},
+					getHtml: function () {
+						return this.template(this.shortcode.attrs.named);
+					}
+				})
+			}));
+
+		}
+	} );
 
 } )( jQuery );
--- views/options-input-text.php
+++ views/options-input-text.php
@@ -1,11 +1,11 @@
-<?php if(isset($title)): ?>
-	<label class="option-label" for="findthebest_options_<?php echo esc_attr( $field ); ?>"><?php echo esc_html( $title ); ?></label>
+<?php if(isset($vars['title'])): ?>
+	<label class="option-label" for="findthebest_options_<?php echo esc_attr( $vars['field'] ); ?>"><?php echo esc_html( $vars['title'] ); ?></label>
 <?php endif; ?>
 
-	<input id="findthebest_options_<?php echo esc_attr( $field ); ?>" class="regular-text code" name="findthebest_options[<?php echo esc_attr( $field ); ?>]" size="40" type="text" value="<?php echo isset($option) && !empty($option) ? esc_attr( $option ) : (isset($default_value) ? esc_attr( $default_value ) : ''); ?>" <?php disabled( $disabled ); ?>  />
+	<input id="findthebest_options_<?php echo esc_attr( $vars['field'] ); ?>" class="regular-text code" name="findthebest_options[<?php echo esc_attr( $vars['field'] ); ?>]" size="40" type="text" value="<?php echo isset($vars['option']) && !empty($vars['option']) ? esc_attr( $vars['option'] ) : (isset($vars['default_value']) ? esc_attr( $vars['default_value'] ) : ''); ?>" <?php disabled( $vars['disabled'] ); ?>  />
 
-<?php if (isset($description)): ?>
+<?php if (isset($vars['description'])): ?>
 
-	<p class="description"><?php echo $description; ?></p>
+	<p class="description"><?php echo wp_kses_post( $vars['description'] ); ?></p>
 
 <?php endif; ?>
